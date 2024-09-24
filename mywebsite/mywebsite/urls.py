b1852358797c1f3b216228069d95e479c46f3b7a
Frog2022 <zhehao4423@gmail.com>
2024-09-24 19:20:27 +0800

    Add views.index

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.home, name='home'),
     path('', include('myapp.urls')),
 ]
